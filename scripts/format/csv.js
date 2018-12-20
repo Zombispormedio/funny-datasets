2cc0835e0eed79dece5cc37ae0ce7d55f0554887
Xavier Serrano <zombispormedio007@gmail.com>
2018-12-20 10:06:09 +0100

    :sparkles: add support parquet

--- scripts/format/csv.js
+++ scripts/format/csv.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const JSONStream = require('JSONStream')
-const csvStringifyLib = require('csv-stringify');
+const csvStringify = require('csv-stringify');
 
 fs.createReadStream('datasets/hits/data.json')
 .pipe(JSONStream.parse("*"))
-.pipe(csvStringifyLib({ header: true }))
+.pipe(csvStringify({ header: true }))
 .pipe(fs.createWriteStream("dist/hits.csv")); 
--- scripts/format/parquet.js
+++ scripts/format/parquet.js
@@ -1,13 +1,17 @@
-const fs = require('fs');
-const parquet = require("parquetjs");
-const JSONStream = require('JSONStream')
-const Chain = require('stream-chain');
+const fs = require("fs");
+const JSONStream = require("JSONStream");
+const { ParquetSchema } = require("parquetjs");
+const { createParquetWriteStream } = require("../../lib");
 
-const schema = new parquet.ParquetSchema({
+const schema = new ParquetSchema({
   title: { type: "UTF8" },
   artist: { type: "UTF8" },
-  feat: { type: "UTF8" },
+  feat: { type: "UTF8", optional: true },
   releaseDate: { type: "UTF8" },
-  album: { type: "UTF8" },
-  url: { type: "UTF8" },
+  album: { type: "UTF8", optional: true },
+  url: { type: "UTF8" }
 });
+
+fs.createReadStream("datasets/hits/data.json")
+  .pipe(JSONStream.parse("*"))
+  .pipe(createParquetWriteStream(schema, "dist/hits.parquet"));
