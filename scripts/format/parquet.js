18fd2ef3be3f288aeb4126375968d3fb8d95b3e7
Xavier Serrano <zombispormedio007@gmail.com>
2018-12-20 10:34:10 +0100

    :recycle: extract parquet schema

--- scripts/format/parquet.js
+++ scripts/format/parquet.js
@@ -2,15 +2,9 @@ const fs = require("fs");
 const JSONStream = require("JSONStream");
 const { ParquetSchema } = require("parquetjs");
 const { createParquetWriteStream } = require("../../lib");
+const schemaFromJson = require("../../datasets/hits/parquet.schema.json");
 
-const schema = new ParquetSchema({
-  title: { type: "UTF8" },
-  artist: { type: "UTF8" },
-  feat: { type: "UTF8", optional: true },
-  releaseDate: { type: "UTF8" },
-  album: { type: "UTF8", optional: true },
-  url: { type: "UTF8" }
-});
+const schema = new ParquetSchema(schemaFromJson);
 
 fs.createReadStream("datasets/hits/data.json")
   .pipe(JSONStream.parse("*"))
