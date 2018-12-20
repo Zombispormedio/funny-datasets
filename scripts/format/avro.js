8b3b0aa780404eae0fc302f76faf9af7094e27e3
Xavier Serrano <zombispormedio007@gmail.com>
2018-12-20 16:38:23 +0100

    :sparkles: feat: support avro

---
+++ scripts/format/avro.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const JSONStream = require("JSONStream");
+const { createFileEncoder } = require('avsc');
+const schema = require("../../datasets/hits/avro.schema.json");
+
+fs.createReadStream("datasets/hits/data.json")
+  .pipe(JSONStream.parse("*"))
+  .pipe(createFileEncoder("dist/hits.avro", schema)) 
--- scripts/reverse/parquet.js
+++ scripts/reverse/parquet.js
@@ -4,4 +4,4 @@ const { createParquetReadStream } = require("../../lib");
 
 createParquetReadStream("dist/hits.parquet")
   .pipe(JSONStream.stringify())
-  .pipe(fs.createWriteStream("dist/hit.from.parquet.json"));
+  .pipe(fs.createWriteStream("dist/hits.from.parquet.json"));
